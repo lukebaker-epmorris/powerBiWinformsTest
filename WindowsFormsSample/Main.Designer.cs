16be131ee621683bbfd737d639157f051ffdc15d
Luke Baker <lukebaker@hotmail.com>
2017-08-09 17:16:30 +0100

    Add project files.

---
+++ WindowsFormsSample/Main.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace WindowsFormsSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.ShowReport = new System.Windows.Forms.Button();
+            this.cboxReports = new System.Windows.Forms.ComboBox();
+            this.LoadReports = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.ShowReport);
+            this.panel1.Controls.Add(this.cboxReports);
+            this.panel1.Controls.Add(this.LoadReports);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(628, 71);
+            this.panel1.TabIndex = 14;
+            // 
+            // ShowReport
+            // 
+            this.ShowReport.Location = new System.Drawing.Point(350, 21);
+            this.ShowReport.Name = "ShowReport";
+            this.ShowReport.Size = new System.Drawing.Size(75, 23);
+            this.ShowReport.TabIndex = 26;
+            this.ShowReport.Text = "Show Report";
+            this.ShowReport.UseVisualStyleBackColor = true;
+            this.ShowReport.Click += new System.EventHandler(this.showReport_Click);
+            // 
+            // cboxReports
+            // 
+            this.cboxReports.FormattingEnabled = true;
+            this.cboxReports.Location = new System.Drawing.Point(167, 21);
+            this.cboxReports.Name = "cboxReports";
+            this.cboxReports.Size = new System.Drawing.Size(177, 21);
+            this.cboxReports.TabIndex = 25;
+            // 
+            // LoadReports
+            // 
+            this.LoadReports.Location = new System.Drawing.Point(12, 19);
+            this.LoadReports.Name = "LoadReports";
+            this.LoadReports.Size = new System.Drawing.Size(149, 23);
+            this.LoadReports.TabIndex = 24;
+            this.LoadReports.Text = "Get report list && token";
+            this.LoadReports.UseVisualStyleBackColor = true;
+            this.LoadReports.Click += new System.EventHandler(this.LoadReports_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.webBrowser1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 71);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(628, 402);
+            this.panel2.TabIndex = 15;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(628, 402);
+            this.webBrowser1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 473);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PowerBI - New API";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button LoadReports;
+        private System.Windows.Forms.Button ShowReport;
+        private System.Windows.Forms.ComboBox cboxReports;
+    }
+}
+
---
+++ WindowsFormsSample/Main.cs
@@ -0,0 +1,195 @@
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.PowerBI.Api.V2;
+using Microsoft.PowerBI.Api.V2.Models;
+using Microsoft.Rest;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Windows.Forms;
+using IMAD = Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+namespace WindowsFormsSample
+{
+    public partial class Form1 : Form
+    {
+        // The Client ID is used by the application to uniquely identify itself to Azure AD.
+        // The Authority is the sign-in URL of the tenant.
+
+        private string clientId = ConfigurationSettings.AppSettings["ClientId"];
+        private string authority = "https://login.windows.net/common/oauth2/authorize";
+
+        // To authenticate to the service, the client needs to know the service's App ID URI.
+        // To contact the list service we need it's URL as well.
+
+        private string resourceId = ConfigurationSettings.AppSettings["ResourceId"];
+        private string listBaseAddress = ConfigurationSettings.AppSettings["BaseAddress"];
+
+        private HttpClient httpClient = new HttpClient();
+        private IMAD.AuthenticationContext authContext = null;
+        private UserCredential uc = new UserPasswordCredential("powerbi@markowenjoneshotmail.onmicrosoft.com", "PasswordForAccount");
+
+        private string AccessToken { get; set; }
+
+        //Power BI Reports used to deserialize the Get Reports response.
+        public class PBIReports
+        {
+            public PBIReport[] value { get; set; }
+        }
+        public class PBIReport
+        {
+            public string id { get; set; }
+            public string name { get; set; }
+            public string webUrl { get; set; }
+            public string embedUrl { get; set; }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            authContext = new IMAD.AuthenticationContext(authority, new FileCache());
+        }
+
+        private void getReportsWithPowerBiApi(string aadToken, string baseUri)
+        {
+            var credentials = new TokenCredentials(aadToken, "Bearer");
+
+            try
+            {
+                using (var client = new PowerBIClient(new Uri(baseUri), credentials))
+                {
+                    var reports = client.Reports.GetReports();
+                    var powerBiReports = reports.Value.ToList();
+                }
+            }
+            catch(Exception ex)
+            {
+                //Operation returned an invalid status code 'Forbidden' 
+            }
+        }
+
+        private string generateEmbedToken(string aadToken, string reportId, string baseUri, string accessLevel = "View", string datasetId = "")
+        {
+            var credentials = new TokenCredentials(aadToken);
+
+            try
+            {
+                using (var powerBiClient = new PowerBIClient(credentials))
+                {
+                    powerBiClient.BaseUri = new Uri(baseUri);
+
+                    var requestParameters = new GenerateTokenRequest(accessLevel, datasetId);
+
+                    
+                    EmbedToken token = powerBiClient.Reports.GenerateToken(reportId, requestParameters);
+
+                    return token.Token;
+                }
+            }
+            catch(Exception ex)
+            {
+                //Operation returned an invalid status code 'Forbidden'
+                return string.Empty;
+            }
+        }
+
+        private void LoadReports_Click(object sender, EventArgs e)
+        {
+            var baseUri = "https://api.powerbi.com/v1.0/myorg/reports";
+
+            var aadToken = newGetToken();
+
+            //example report ID
+            AccessToken = generateEmbedToken(aadToken, "aa0fabeb-f4d0-4d9b-a109-d1db031f455e", baseUri); //forbidden
+
+            System.Net.WebRequest request = System.Net.WebRequest.Create(baseUri) as System.Net.HttpWebRequest;
+
+            request.Method = "GET";
+            request.ContentType = "application/json";
+            request.ContentLength = 0;
+
+            request.Headers.Add("Authorization", String.Format("Bearer {0}", AccessToken));
+            try
+            {
+
+                //Get Reports response from request.GetResponse()
+                using (var response = request.GetResponse() as System.Net.HttpWebResponse) //403 error
+                {
+                    //Get reader from response stream
+                    using (var reader = new System.IO.StreamReader(response.GetResponseStream()))
+                    {
+                        //Deserialize JSON string
+                        PBIReports Reports = JsonConvert.DeserializeObject<PBIReports>(reader.ReadToEnd());
+
+                        cboxReports.DataSource = null;
+                        cboxReports.Items.Clear();
+
+                        List<KeyValuePair<string, string>> menuItems = new List<KeyValuePair<string, string>>();
+                        BindingSource bs = new BindingSource(menuItems, null);
+
+                        cboxReports.DataSource = bs;
+                        cboxReports.DisplayMember = "Value";
+                        cboxReports.ValueMember = "Key";
+
+                        for (int i = 0; i < Reports.value.Length; i++)
+                        {
+                            menuItems.Add(new KeyValuePair<string, string>(Reports.value[i].embedUrl, Reports.value[i].name));
+                            bs.ResetBindings(false);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //Getting 403 forbidden
+            }
+
+        }
+
+        private void WebBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (AccessToken != null)
+            {
+                var message = "{\"action\":\"loadReport\",\"accessToken\":\"" + AccessToken + "\"}";
+                webBrowser1.Document.InvokeScript("postMessage", new object[] { message, "*" });
+            }
+            else
+            {
+                MessageBox.Show("AccessToken is not defined.");
+            }
+        }
+
+        private string newGetToken()
+        {
+            string token = string.Empty;
+
+            try
+            {
+                IMAD.AuthenticationResult result = authContext.AcquireTokenAsync(resourceId, clientId, uc).Result;
+                token = result.AccessToken;
+            }
+            catch (AdalException ex)
+            {
+                // An unexpected error occurred.
+                string message = ex.Message;
+                if (ex.InnerException != null)
+                {
+                    message += "Error Code: " + ex.ErrorCode + "Inner Exception : " + ex.InnerException.Message;
+                }
+                MessageBox.Show(message);
+            }
+            return token;
+        }
+
+        private void showReport_Click(object sender, EventArgs e)
+        {
+            var report = cboxReports.SelectedValue.ToString();
+            webBrowser1.DocumentCompleted -= WebBrowser1_DocumentCompleted;
+            webBrowser1.DocumentCompleted += WebBrowser1_DocumentCompleted;
+            webBrowser1.Navigate(report + "&filterPaneEnabled=false");
+        }
+
+    }
+}
